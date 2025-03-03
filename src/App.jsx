b1c9a27197b4916cb099a669f25b14b73799ba91
Chun <mevius92201@gmail.com>
2025-03-03 15:29:56 +0800

    Add LoginForm and RegisterForm components with authentication functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,145 +1,28 @@
-import { useState, useEffect, useRef } from "react";
-import { useForm } from "react-hook-form";
+import { useState, useEffect } from "react";
 import AuthPage from "./Component/AuthPage.jsx";
-import Icon from './Component/Icon';
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/all.css"
+import "./assets/all.css";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import LoadingEffect from './Component/LoadingEffect.jsx'
+import LoadingEffect from "./Component/LoadingEffect.jsx";
 import CartPage from "./Component/CartPage.jsx";
+import ProductDisplayPage from "./Component/ProductDisplayPage.jsx";
+import PaymentForm from "./Component/PaymentForm.jsx";
 const API_BASE = "https://ec-course-api.hexschool.io/v2";
 const API_PATH = "mevius";
 function App() {
-  const { register, handleSubmit, watch, reset, formState, formState: { errors, isSubmitSuccessful } } = 
-  useForm({
-    mode: 'onTouched',
-    defaultValues: {
-      email: "",
-      name: "",
-      tel: "",
-      address: "",
-      message: ""
-    }
-  });
-  const [productsData, setProductsData] = useState([]);
-  const [cartProductData, setCartProductData]= useState([]);
+  const [cartProductData, setCartProductData] = useState([]);
   const [cartChanged, setCartChanged] = useState(false);
-  console.log(watch())
-  const [hoveredProduct, setHoveredProduct] = useState(null);
-  const [clickedProduct, setClickedProduct] = useState(null);
-  const [cardInfoPosition, setCardInfoPosition] = useState('right');
   const [loading, setLoading] = useState(false);
-  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
-  
+  const [productsData, setProductsData] = useState([]);
+
   const getProduct = async () => {
-  try {
-    const res = await axios.get(`${API_BASE}/api/${API_PATH}/products/all`);
-    setProductsData(res.data.products);
-  } catch (err) {
-    toast.error(err.response.data.message,{
-      position: "top-center",
-      autoClose: 1500,
-      hideProgressBar: true,
-      closeOnClick: true,
-      pauseOnHover: false,
-      draggable: false,
-      theme: "colored",
-  })
-  }
-  };
-  useEffect(() => {
-    getProduct();
-  }, []);
-
-  const addProductToCart = async (productId) => {
-    if (isButtonDisabled) return;
-    try{
-      setIsButtonDisabled(true);
-      await axios.post(`${API_BASE}/api/${API_PATH}/cart`,
-      {
-        "data": {
-          "product_id": productId,
-          "qty": 1
-        }
-      }
-    )
-    toast.success("商品已加入購物車", {
-      position: "top-center",
-      autoClose: 1500,
-      hideProgressBar: true,
-      closeOnClick: true,
-      pauseOnHover: false,
-      draggable: false,
-      theme: "colored",
-    });
-    setCartChanged(!cartChanged);
-    setTimeout(() => setIsButtonDisabled(false), 1000);
-    }catch(err){
-      toast.error(err.response.data.message,{
-        position: "top-center",
-        autoClose: 1500,
-        hideProgressBar: true,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: false,
-        theme: "colored",
-    })
-    }
-  }
-  
-  const onSubmit = async(data) =>{
-    try{
-      
-      setLoading(true);
-    const res = await axios.post(`${API_BASE}/api/${API_PATH}/order`,{
-      "data": {
-        "user": {
-          "name": data.name,
-        "email": data.email,
-          "tel": data.tel,
-          "address": data.address,
-        },
-        "message": data.message,
-      }
-    })
-    console.log(res.data.orderId);
-    await orderPaid(res.data.orderId);
-    setCartChanged(!cartChanged);
-    }catch(err){
-      toast.error(err.response.data.message,{
-        position: "top-center",
-        autoClose: 1500,
-        hideProgressBar: true,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: false,
-        theme: "colored",
-    })
-    }finally{
-      setTimeout(() => setLoading(false), 500);
-    }
-  }
-
-  const orderPaid = async(orderId)=>{
-    try{
-      await axios.post(`${API_BASE}/api/${API_PATH}/pay/${orderId}`)
-    }catch(err){
-      toast.error(err.response.data.message,{
-        position: "top-center",
-        autoClose: 1500,
-        hideProgressBar: true,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: false,
-        theme: "colored",
-    })
-    }
-  }
-  useEffect (() => {
-    if(isSubmitSuccessful){
-      toast.success("訂單已送出", {
+    try {
+      const res = await axios.get(`${API_BASE}/api/${API_PATH}/products/all`);
+      setProductsData(res.data.products);
+    } catch (err) {
+      toast.error(err.response.data.message, {
         position: "top-center",
         autoClose: 1500,
         hideProgressBar: true,
@@ -148,110 +31,23 @@ function App() {
         draggable: false,
         theme: "colored",
       });
-      reset({email: "",
-        name: "",
-        tel: "",
-        address: "",
-        message: ""});
-    }
-  },[isSubmitSuccessful])
-
-  const handleMouseEnter = (e) => {
-    const cardRect = e.target.getBoundingClientRect();
-    console.log("cardRect", cardRect.right, window.innerWidth);
-    if (cardRect.right > window.innerWidth/1.3) {
-      setCardInfoPosition('left'); 
-    } else {
-      setCardInfoPosition('right');
     }
   };
+  useEffect(() => {
+    getProduct();
+  }, []);
 
   return (
     <>
-     <AuthPage getProducts={getProduct} setIsAuth={false} />
-    <div id="app" >
-      <div className="container">
-          {/* 產品Modal */}
-          <section className="product-board bg_01">
-          {productsData.map((product,index) => (
-          <div className="product-card"
-          key={index}
-          onMouseEnter={handleMouseEnter}>
-              <div className="product-card-body">
-                <div className="product-title">{product.title}</div>
-                <Icon type="icon-frame" />
-                <div style={
-                  {backgroundImage: `url(${product?.imageUrl})`}} 
-                  className="product-main-img" alt="..." />
-                <div className="product-price-display">
-                  <Icon type="icon-CP" style={{ marginRight: '8px' }} />
-                  {product.origin_price > product.price ?
-                  (<><del style={{fontSize: ".8rem", paddingRight: ".2rem"}}>{product.origin_price}</del><div style={{color: "#000"}}>{product.price}</div></> ):
-                  (<div style={{color:"#000"}}>{product.price}</div>)}
-                </div>            
-                <div className ="product-card-body-mask">
-                  <div className ="product-info-hovered">
-                  <div className="generate-info-btn"
-                    onMouseEnter={() => setHoveredProduct(product.id)}
-                    onMouseLeave={() => setHoveredProduct(null)}
-                    
-                    onClick={() =>
-                    setClickedProduct((prev) => (prev === product.id ? null : product.id))
-                    }
-                  >
-                    <img className ="generate-info-icon" src="../src/assets/icons/eye.png" alt="..." />
-                  查看
-                  </div>
-                  <div className={`product-info-board product-info-board-${cardInfoPosition}`}
-                  // style={{ display: hoveredProduct === product.id || clickedProduct === product.id
-                  //       ? "block"
-                  //       : "none",
-                  // }}> //他照成畫面閃爍的混亂ㄌ凸
-                  >
-                    <div className="product-info-container">
-                      <div className="product-info-card">
-                        <div className="product-info-card-content">
-                          <div className="product-info">
-                            <div className="product-info-title h5">INFO</div>
-                            <div className="product-info-product-name">商品：<span style={{color:"#f5e1fdc4"}}>{product.title}</span></div>
-                            <div className="product-info-product-category">分類：<span style={{color:"#f5e1fdc4"}}>{product.category}</span></div>
-                            <div className="product-info-product-category">說明：<span style={{color:"#f5e1fdc4"}}>{product.description}</span></div>
-                            <div className="product-info-product-content">
-                              <div className="product-info-product-content-title">
-                                <span>商品描述</span>
-                              </div>
-                              <div className="product-info-product-content-content">
-                                <span>{product.content}</span>
-                              </div>
-                            </div>
-                            <div className="product-info-price-display">售價：
-                              <Icon type="icon-CP" style={{ marginRight: '8px' }} />
-                              {product.origin_price > product.price ?
-                              (<><del style={{fontSize: ".8rem", paddingRight: ".2rem"}}>{product.origin_price}</del><div>{product.price}</div></> ):
-                              (<div>{product.price}</div>)}
-                            </div>
-                            {/* <div className="product-info-product-thumbnail">
-                              {(product.imagesUrl.map((item)=><><img src=item alt="..."/></>))} 
-                            </div> */}
-                          </div>
-                        </div>
-                        <div className="product-add-cart">
-                          <button className={`product-add-cart-btn 
-                          ${isButtonDisabled ? 'btn-disabled' : ''}`}
-                          type="button"
-                          onClick={() => addProductToCart(product.id)}
-                          disabled={isButtonDisabled}
-                          >加入購物車</button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  </div>
-                </div>
-              </div>
-          </div>
-          ))}
-          </section>
+      <AuthPage getProducts={getProduct} setIsAuth={false} />
+      <div id="app">
+        <div className="container">
+          <ProductDisplayPage
+            productsData={productsData}
+            cartChanged={cartChanged}
+            setCartChanged={setCartChanged}
+          />
+
           {/* <table className="table align-middle">
             <thead>
               <tr>
@@ -286,7 +82,7 @@ function App() {
                     <button type="button" className="btn btn-outline-danger"
                     onClick={() => addProductToCart(product.id)}>
                       {/* <i className="fas fa-spinner fa-pulse"></i> */}
-                      {/* 加到購物車
+          {/* 加到購物車
                     </button>
                   </div>
                 </td>
@@ -294,133 +90,26 @@ function App() {
               ))}
             </tbody>
           </table> */}
-          < CartPage 
+          <CartPage
             cartChanged={cartChanged}
             setCartChanged={setCartChanged}
             cartProductData={cartProductData}
             setCartProductData={setCartProductData}
             setLoading={setLoading}
           />
-        <div className="my-5 row justify-content-center">
-          <form 
-          className="col-md-6"
-          onSubmit={handleSubmit(onSubmit)}
-          >
-            <div className="mb-3">
-              <label htmlFor="email" className="form-label">Email</label>
-              <input id="email"
-              name="email" 
-              type="email" 
-              {...register('email',{
-                required: {
-                  value: true,
-                  message: "此欄位必填"
-                },
-                pattern: {
-                  value: /^\S+@\S+\.\S+$/i,
-                  message: "請輸入正確的 Email 格式"
-                }
-              })}
-              className={`form-control ${errors.email ? 'is-invalid' : ''}`}
-              placeholder="請輸入 Email" />
-              {errors.email && <div className="invalid-hint">{errors?.email?.message}</div>}
-            </div>
-            
-            <div className="mb-3">
-              <label htmlFor="name" className="form-label">收件人姓名</label>
-              <input id="name" 
-              name="姓名" 
-              type="text" 
-              {...register('name',{
-                required: {
-                  value: true,
-                  message: "此欄位位必填"
-                },
-                minLength: {
-                  value: 2,
-                  message: "請輸入至少2個字"
-                },
-                maxLength: {
-                  value: 20,
-                  message: "請勿超過20個字"
-                }
-              })}
-              className="form-control" 
-              placeholder="請輸入姓名" />
-              {errors.name && <div className="invalid-hint">{errors?.name?.message}</div>}
-            </div>
-
-            <div className="mb-3">
-              <label htmlFor="tel" className="form-label">收件人電話</label>
-              <input id="tel" 
-              name="電話" 
-              type="text" 
-              {...register('tel',{
-                required: {
-                  value: true,
-                  message: "此欄位必填"
-                },
-                pattern: {
-                  value: /^09\d{8}$|^09\d{2}-\d{3}-\d{3}$/,
-                  message: "請輸入正確的手機格式"
-                }
-              })}
-              className="form-control" 
-              placeholder="請輸入電話" />
-              {errors.tel && <div className="invalid-hint">{errors?.tel?.message}</div>}
-            </div>
-
-            <div className="mb-3">
-              <label htmlFor="address" className="form-label">收件人地址</label>
-              <input id="address" 
-              name="地址" 
-              type="text" 
-              {...register('address',{
-                required: {
-                  value: true,
-                  message: "此欄位位必填"
-                },
-                minLength:{
-                  value: 5,
-                  message: "請輸入至少5個字"
-                },
-                maxLength:{
-                  value: 150,
-                  message: "請勿超過150個字"
-                }
-              })}
-              className="form-control" 
-              placeholder="請輸入地址" />
-              {errors.address && <div className="invalid-hint">{errors?.address?.message}</div>}
-            </div>
-
-            <div className="mb-3">
-              <label htmlFor="message" className="form-label">留言</label>
-              <textarea id="message" 
-              className="form-control" 
-              {...register('message',{
-                maxLength:{
-                  value: 300,
-                  message: "請勿超過300個字"
-                }
-              }
-              )}
-              cols="30" 
-              rows="10"></textarea>
-            </div>
-            <div className="text-end">
-              <button type="submit" className="btn btn-danger"
-              disabled={cartProductData.length === 0}>送出訂單</button>
-            </div>
-          </form>
+          <PaymentForm
+            setLoading={setLoading}
+            cartChanged={cartChanged}
+            setCartChanged={setCartChanged}
+            cartProductData={cartProductData}
+          />
         </div>
       </div>
-    </div>
-    <div>
-      <ToastContainer />
-      <LoadingEffect loadingState={loading}/>
-    </div>
- </>
-  )
+      <div>
+        <ToastContainer />
+        <LoadingEffect loadingState={loading} />
+      </div>
+    </>
+  );
 }
-export default App
+export default App;
--- src/Component/AuthPage.jsx
+++ src/Component/AuthPage.jsx
@@ -1,8 +1,7 @@
-import axios from "axios";
 import { useState } from 'react';
-import { useForm } from "react-hook-form";
-
-const API_BASE = "https://ec-course-api.hexschool.io/v2";
+import PropTypes from 'prop-types';
+import RegisterForm from "./RegisterForm";
+import LoginForm from './LoginForm';
 
 const AuthPage=({ getProducts, setIsAuth })=>{
   const [tab, setTab] = useState("login")
@@ -38,167 +37,10 @@ const AuthPage=({ getProducts, setIsAuth })=>{
   )
 }
 
-const LoginForm = ({getProducts, setIsAuth}) =>{
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const [passwordType, setPasswordType] = useState("password");
-
-  const hasPasswordShow = () => {
-    setPasswordType(prev => (prev === "password" ? "text" : "password"));
-  };
-  const onSubmit = async (data) => {
-    try {
-      const res = await axios.post(`${API_BASE}/admin/signin`, data);
-      const { token, expired } = res.data;
-      document.cookie = `access_token=${token};expires=${new Date(expired)};`;
-      axios.defaults.headers.common.Authorization = token;
-      setIsAuth(true);
-      getProducts();
-    } catch (error) {
-      alert("登入失敗: " + error.response.data.message);
-    }
-  };
-
-return (
-  <form className="auth-form"
-  onSubmit={handleSubmit(onSubmit)}>
-    <div className="input-fields">
-      <div className ="floating">
-        <div className="floating__inner">
-          <input 
-          id="signInUsername"
-          className={errors.email? "error_auth-input" : "auth-input"}
-          {...register('email',{
-      required: {
-        value: true,
-        message: "此欄位必填"
-      },
-      pattern: {
-        value: /^\S+@\S+\.\S+$/i,
-        message: "請輸入正確的 Email 格式"
-      }
-          })} 
-          placeholder="" />
-          <label htmlFor="signInUsername" className="userName">Email</label>
-        </div>
-          {errors.email && <div className="error-hint">{errors?.email?.message}</div>}
-      </div>
-      <div className ="floating">
-        <div className="floating__inner">
-          <input id="signInPassword"
-          className={errors.password? "error_auth-input" : "auth-input"}
-          {...register("password",{ 
-          required: "請輸入密碼" })} 
-          type={passwordType} 
-          placeholder="" />
-          <label htmlFor="signInPassword" className="passWord">Password</label>
-        </div>
-        {errors.password && <div className="error-hint">{errors?.password?.message}</div>}
-      </div>
-    </div>
-    <label className="form-check-label">
-      <input type="checkbox"
-      className="form-check-input"
-      onChange={hasPasswordShow} /> 顯示密碼
-    </label>
-    <button type="submit" className="signin-btn">登入</button>
-  </form>
-  )
-}
-
-             
-const RegisterForm = ({getProducts, setIsAuth}) =>{
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const [passwordType, setPasswordType] = useState("password");
-  const hasPasswordShow = () => {
-    setPasswordType(prev => (prev === "password"? "text" : "password"));
-  };
-  const onSubmit = async (data) => {
-    try {
-      const res = await axios.post(`${API_BASE}/admin/signup`, data);
-      const { token, expired } = res.data;
-      document.cookie = `access_token=${token};expires=${new Date(expired)};`;
-      axios.defaults.headers.common.Authorization = token;
-      setIsAuth(true);
-      getProducts();
-    } catch (error) {
-      alert("註冊失敗: " + error.response.data.message);
-    }
-  };
-  return(
-    <form className="auth-form"
-    onSubmit={handleSubmit(onSubmit)}>
-      <div className="input-fields">
-        <div className ="floating">
-          <div className="floating__inner">
-            <input id="createUsername"
-            className={errors.email? "error_auth-input" : "auth-input"}
-            {...register('email',{
-              required: {
-                value: true,
-                message: "請填入信箱"
-              },
-              pattern: {
-                value: /^\S+@\S+\.\S+$/i,
-                message: "請輸入正確的Email格式"
-              }
-            })} 
-            placeholder="" />
-            <label htmlFor="createUsername" className="userName">Email</label>
-          </div>
-          {errors.email && <div className="error-hint">{errors?.email?.message}</div>}
-        </div>
-        <div className ="floating">
-          <div className="floating__inner">
-          <input id="createPassword"
-          className={errors.password? "error_auth-input" : "auth-input"}
-          {...register("password",{
-            required: {
-              value: true,
-              message: "此欄位位必填"
-            },
-            minLength: {
-              value: 6,
-              message: "密碼需要至少6字元"
-            },
-            maxLength: {
-              value: 20,
-              message: "不要超過20字元^^"
-            }
-            })
-          }
-          placeholder=""
-          type={passwordType} />
-          <label htmlFor="createPassword" className="passWord">Password</label>
-          </div>
-          {errors.password && <div className="error-hint">{errors?.password?.message}</div>}
-        </div>
-        <div className ="floating">
-          <div className="floating__inner">
-            <input id="createConfirmPassword"
-            className={errors.confirmPassword? "error_auth-input" : "auth-input"}
-            {...register("confirmPassword",{
-              required: {
-                value: true,
-                message: "請確認密碼"
-              },
-              validate: (value) => value === watch("password") || "密碼一樣嗎？"
-            })}
-            placeholder=""
-            type={passwordType} />
-          <label htmlFor="createConfirmPassword" className="passWord">Confirm Password</label>
-          </div>
-          {errors.confirmPassword && <div className="error-hint">{errors?.confirmPassword?.message}</div>}
-        </div>
-      </div>
-      <label className="form-check-label">
-        <input type="checkbox" 
-        className="form-check-input"
-        onChange={hasPasswordShow} /> 顯示密碼
-      </label>
-      <button type="submit" className="create-btn">註冊</button>
-    </form>
-  )
-}
+AuthPage.propTypes = {
+    setIsAuth: PropTypes.bool.isRequired,
+    getProducts: PropTypes.func.isRequired,
+};
   export default AuthPage
 // function LoginPage({getProducts, setIsAuth}) {
 
